1cd0a4e7ac9f0bdcae053eb3a3ec113152f10791
William Emfinger <waemfinger@gmail.com>
2017-04-18 09:43:04 -0700

    update for bugged include.

--- src/rosmod_actor/src/rosmod_actor/main.cpp
+++ src/rosmod_actor/src/rosmod_actor/main.cpp
@@ -11,8 +11,8 @@
 #include <cstdlib>        
 #include <signal.h>
 #include <boost/thread.hpp>
-#include "rosmod_actor/Component.hpp"
-#include "json/json.h"
+#include "rosmod_component/Component.hpp"
+#include "rosmod_jsoncpp/json.h"
 #include "pthread.h"
 #include "sched.h"
 #include <iostream>
